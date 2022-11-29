63bfce41e448cf09c2478168998b1752345e1a0f
szucs-kata <115154358+szucs-kata@users.noreply.github.com>
2022-11-29 16:42:07 +0100

    Update main.js
    rgb kódok törlése

--- network/js/main.js
+++ network/js/main.js
@@ -608,7 +608,7 @@ function showCluster(a) {
         }
         sigInst.clusters[a] = e;
         sigInst.draw(2, 2, 2, 2);
-        $GP.info_name.html("<b>" + a + "</b>");
+        $GP.info_name.html(false);
         $GP.info_data.hide();
         $GP.info_p.html("Levélírók:");
         $GP.info_link.find("ul").html(f.join(""));
